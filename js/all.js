67b3e21d7520e9b347c71429979716379e8fcf23
Yiren-Liou <liou830129@gmail.com>
2020-05-27 21:01:33 +0800

    Initial commit

---
+++ js/all.js
@@ -0,0 +1,16 @@
+// $(document).ready(function() {
+//   AOS.init({
+//     useClassNames: true,
+//     initClassName: false,
+//     animatedClassName: 'animate__animated',
+//   });
+// });
+
+AOS.init({
+  useClassNames: true,
+  initClassName: false,
+  animatedClassName: 'animate__animated',
+});
+
+
+
